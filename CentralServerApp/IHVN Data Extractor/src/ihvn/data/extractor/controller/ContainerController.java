da30fbd184b2c2d80b64e0d8e68b92095a9d8699
The Bright <The Bright@BRIGHTOIBE>
2020-05-31 22:44:22 +0100

    Checked for null pointer during encryption

--- CentralServerApp/IHVN Data Extractor/src/ihvn/data/extractor/controller/ContainerController.java
+++ CentralServerApp/IHVN Data Extractor/src/ihvn/data/extractor/controller/ContainerController.java
@@ -34,29 +34,25 @@ import javax.xml.datatype.XMLGregorianCalendar;
  * @author rsuth
  */
 public class ContainerController {
-    
+
     Map<String, String> patientDetails;
-    
-    public ContainerController(Map<String, String> patientDetails)
-    {
+
+    public ContainerController(Map<String, String> patientDetails) {
         this.patientDetails = patientDetails;
     }
-    
+
     //build container
-    public Container buildContainer()
-    {
+    public Container buildContainer() {
         Container container = new Container();
         container.setMessageHeader(this.buildMessageHeader());
         container.setMessageData(this.buildMessageData());
-        
+
         return container;
     }
-    
-    
+
     //build message header type
-    private MessageHeaderType buildMessageHeader()
-    {
-        
+    private MessageHeaderType buildMessageHeader() {
+
         MessageHeaderType messageHeader = new MessageHeaderType();
         messageHeader.setFacilityDatimCode(MainController.datimId);
         messageHeader.setFacilityName(MainController.patientFacilityName);
@@ -67,138 +63,142 @@ public class ContainerController {
         messageHeader.setMessageSource("NMRS");
         return messageHeader;
     }
-    
+
     //build message data
-    private MessageDataType buildMessageData()
-    {
+    private MessageDataType buildMessageData() {
         MessageDataType messageData = new MessageDataType();
         messageData.setDemographics(this.buildDemographics());
         messageData.getVisits().addAll(this.buildVisits());
-        
+
         return messageData;
-        
+
     }
-    
+
     //build demographics 
-    private DemographicsType buildDemographics()
-    {
+    private DemographicsType buildDemographics() {
         DemographicsType demo = new DemographicsType();
-        try{
-            
-            demo.setAddress1(Misc.encrypt(patientDetails.get("address1")));
-            demo.setAddress2(Misc.encrypt(patientDetails.get("address2")));
+        try {
+            if (patientDetails.get("address1") != null) {
+                demo.setAddress1(Misc.encrypt(patientDetails.get("address1")));
+            }
+            if (patientDetails.get("address2") != null) {
+                demo.setAddress2(Misc.encrypt(patientDetails.get("address2")));
+            }
+
             demo.setCityVillage(patientDetails.get("cityVillage"));
             demo.setStateProvince(patientDetails.get("stateProvince"));
             demo.setCountry(patientDetails.get("country"));
-            demo.setVoided(Integer.parseInt(patientDetails.get("voided")));
-            demo.setVoidedBy(Integer.parseInt(patientDetails.get("voidedBy")));
-            if(patientDetails.get("dateVoided")!=null){
-               demo.setDateVoided(Misc.getXMLdate(new SimpleDateFormat("yyyy-MM-dd").parse(patientDetails.get("dateVoided")))); 
+            if (patientDetails.get("voided") != null) {
+                demo.setVoided(Integer.parseInt(patientDetails.get("voided")));
+            }
+            if (patientDetails.get("voidedBy") != null) {
+                demo.setVoidedBy(Integer.parseInt(patientDetails.get("voidedBy")));
+            }
+            if (patientDetails.get("dateVoided") != null) {
+                demo.setDateVoided(Misc.getXMLdate(new SimpleDateFormat("yyyy-MM-dd").parse(patientDetails.get("dateVoided"))));
             }
             demo.setCauseOfDeath(patientDetails.get("causeOfDeath"));
-            demo.setCreator(Integer.parseInt(patientDetails.get("creator")));
-            if(patientDetails.get("dateCreated")!=null){
-               demo.setDateCreated(Misc.getXMLdate(new SimpleDateFormat("yyyy-MM-dd").parse(patientDetails.get("dateCreated"))));  
+            if (patientDetails.get("creator") != null) {
+                demo.setCreator(Integer.parseInt(patientDetails.get("creator")));
+            }
+
+            if (patientDetails.get("dateCreated") != null) {
+                demo.setDateCreated(Misc.getXMLdate(new SimpleDateFormat("yyyy-MM-dd").parse(patientDetails.get("dateCreated"))));
             }
             demo.setVoidedReason(patientDetails.get("voidReason"));
             demo.setDeathdateEstimated(Integer.parseInt(patientDetails.get("deathdateEstimated")));
-            
-            
+
             XMLGregorianCalendar dob = null;
             XMLGregorianCalendar deathDate = null;
-            if(patientDetails.get("birthDate") != null)
-            {
+            if (patientDetails.get("birthDate") != null) {
                 dob = Misc.getXMLdate(new SimpleDateFormat("yyyy-MM-dd").parse(patientDetails.get("birthDate")));
             }
-           
+
             demo.setBirthdate(dob);
-            demo.setBirthdateEstimated(Integer.parseInt(patientDetails.get("birthDateEstimated")));
-            demo.setCityVillage("");
-            demo.setCountry("");
-            demo.setDead(Integer.parseInt(patientDetails.get("dead")));
-             if(patientDetails.get("deathDate") != null)
-            {
+            if (patientDetails.get("birthDateEstimated") != null) {
+                demo.setBirthdateEstimated(Integer.parseInt(patientDetails.get("birthDateEstimated")));
+            }
+            if (patientDetails.get("dead") != null) {
+                demo.setDead(Integer.parseInt(patientDetails.get("dead")));
+            }
+
+            if (patientDetails.get("deathDate") != null) {
                 deathDate = Misc.getXMLdate(new SimpleDateFormat("yyyy-MM-dd").parse(patientDetails.get("deathDate")));
             }
             demo.setDeathDate(deathDate);
-            demo.setFirstName(Misc.encrypt(patientDetails.get("firstName")));
-            demo.setGender(patientDetails.get("gender"));
-            demo.setLastName(Misc.encrypt(patientDetails.get("lastName")));
-            demo.setMiddleName(Misc.encrypt(patientDetails.get("middleName")));
+            if (patientDetails.get("firstName") != null) {
+                demo.setFirstName(Misc.encrypt(patientDetails.get("firstName")));
+            }
+            if (patientDetails.get("lastName") != null) {
+                demo.setLastName(Misc.encrypt(patientDetails.get("lastName")));
+            }
+            if (patientDetails.get("middleName") != null) {
+                demo.setMiddleName(Misc.encrypt(patientDetails.get("middleName")));
+            }
             
+            if(patientDetails.get("phone")!=null){
+                demo.setPhoneNumber(Misc.encrypt(patientDetails.get("phone")));
+            }
+            demo.setGender(patientDetails.get("gender"));
+
             demo.setPatientUuid(patientDetails.get("patientUUID"));
             demo.setPatientId(Integer.parseInt(patientDetails.get("patientId")));
-            demo.setPhoneNumber(Misc.encrypt(patientDetails.get("phone")));
             
-            
+
             demo.getPatientBiometric().addAll(this.buildPatientBiometrics());
             demo.getPatientIdentifiers().addAll(this.buildPatienIdentifiers());
             demo.getPatientProgram().addAll(this.buildPatientPrograms());
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-            
+
         return demo;
     }
-    
+
     //build visits 
-    private List<VisitType> buildVisits()
-    {
+    private List<VisitType> buildVisits() {
         VisitDAO visitObj = new VisitDAO();
         List<VisitType> allVisits = visitObj.getAllVisits(Integer.parseInt(patientDetails.get("patientId")));
-        
+
         return allVisits;
     }
-    
-    
+
     //build patientBiometrics
-    private List<PatientBiometricType> buildPatientBiometrics()
-    {
+    private List<PatientBiometricType> buildPatientBiometrics() {
         PatientBiometricDAO biometricObj = new PatientBiometricDAO();
         List<PatientBiometricType> allBiometrics = biometricObj.getPatientBiometric(Integer.parseInt(patientDetails.get("patientId")));
-         
+
         return allBiometrics;
     }
-    
+
     //build patient programs
-     private List<PatientProgramType> buildPatientPrograms()
-    {
+    private List<PatientProgramType> buildPatientPrograms() {
         PatientProgramDAO patientProgramObj = new PatientProgramDAO();
         List<PatientProgramType> allPatientPrograms = patientProgramObj.getAllPatientPrograms(Integer.parseInt(patientDetails.get("patientId")));
-        
+
         return allPatientPrograms;
     }
-     
-     
+
     //build patient identifiers
-    private List<PatientIdentifierType> buildPatienIdentifiers()
-    {
+    private List<PatientIdentifierType> buildPatienIdentifiers() {
         PatientIdentifierDAO patientIdentifierObj = new PatientIdentifierDAO();
         List<PatientIdentifierType> allPatientIdentifiers = patientIdentifierObj.getAllPatientIdentifiers(Integer.parseInt(patientDetails.get("patientId")));
-        
+
         return allPatientIdentifiers;
     }
-    
+
     //build encounters 
-    private List<EncounterType> buildEncounters()
-    {
+    private List<EncounterType> buildEncounters() {
         List<EncounterType> allEncounters = new ArrayList<>();
-        
+
         return allEncounters;
     }
-    
+
     //build obs 
-    private List<ObsType> buildObs()
-    {
+    private List<ObsType> buildObs() {
         List<ObsType> allObs = new ArrayList<>();
-        
-        
-        
+
         return allObs;
     }
-    
-    
-    
+
 }
